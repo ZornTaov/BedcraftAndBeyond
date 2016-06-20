ed373aa57823a1d09ead6e0c37d9277e4d6ef467
Ted Senft <ted.senft@gmail.com>
2016-06-20 00:49:25 -0400

    Attempting jei recipe for dye bottles

--- src/main/java/zornco/bedcraftbeyond/compat/CompatLoader.java
+++ src/main/java/zornco/bedcraftbeyond/compat/CompatLoader.java
@@ -1,14 +1,9 @@
 package zornco.bedcraftbeyond.compat;
 
-import net.minecraftforge.fml.common.FMLModContainer;
-import net.minecraftforge.fml.common.Loader;
-import zornco.bedcraftbeyond.compat.jei.JeiCompat;
-
 public abstract class CompatLoader {
 
     public static void loadCompat(){
-        if(Loader.isModLoaded("JEI"))
-            JeiCompat.load();
+
     }
 
 }
--- src/main/java/zornco/bedcraftbeyond/compat/jei/dye_bottle/WrapperDyeBottle.java
+++ src/main/java/zornco/bedcraftbeyond/compat/jei/dye_bottle/WrapperDyeBottle.java
@@ -1,13 +1,36 @@
 package zornco.bedcraftbeyond.compat.jei.dye_bottle;
 
+import com.google.common.collect.ImmutableList;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.asm.transformers.ItemStackTransformer;
+import net.minecraftforge.oredict.OreDictionary;
 import zornco.bedcraftbeyond.common.crafting.recipes.RecipeDyeBottle;
+import zornco.bedcraftbeyond.common.item.BcbItems;
 
-import java.sql.Wrapper;
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WrapperDyeBottle extends BlankRecipeWrapper {
 
     public WrapperDyeBottle(RecipeDyeBottle recipe){
-        
+
+    }
+
+    @Nonnull
+    @Override
+    public List getInputs() {
+        return ImmutableList.of(
+          null, OreDictionary.getOres("dyeRed"), null,
+            OreDictionary.getOres("dyeGreen"), new ItemStack(BcbItems.eyedropper), OreDictionary.getOres("dyeBlue"),
+            null, OreDictionary.getOres("bottleWater"), null
+        );
+    }
+
+    @Nonnull
+    @Override
+    public List getOutputs() {
+        return ImmutableList.of(new ItemStack(BcbItems.dyeBottle));
     }
 }
