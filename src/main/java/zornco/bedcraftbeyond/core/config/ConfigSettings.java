357717af3038d3692f1f936a969e09e3fcfa7d70
Ted Senft <ted.senft@gmail.com>
2016-07-09 22:32:57 -0400

    Add configuration stuffs

---
+++ src/main/java/zornco/bedcraftbeyond/core/config/ConfigSettings.java
@@ -0,0 +1,8 @@
+package zornco.bedcraftbeyond.core.config;
+
+public class ConfigSettings {
+    public static boolean ADD_OREDICT_WOODEN;
+    public static boolean ADD_OREDICT_STONE;
+
+    public static int DRAWER_ITEM_LIMIT;
+}
