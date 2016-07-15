79667d1a24b3bc1e9948ca8506154969fb77da4b
ZornTaov <zeke_kjell2@yahoo.com>
2016-07-14 18:17:56 -0700

    fixed indents in RenderingHelper

--- src/main/java/zornco/bedcraftbeyond/core/util/RenderingHelper.java
+++ src/main/java/zornco/bedcraftbeyond/core/util/RenderingHelper.java
@@ -7,27 +7,27 @@ import net.minecraftforge.client.model.ModelLoader;
 
 public class RenderingHelper {
 
-  public static void registerBlockModelAsItem(final Block block, int meta) {
-    registerBlockModelAsItem(block, meta, "inventory");
-  }
-  public static void registerBlockModelAsItem(final Block block, int meta, final String variantName) {
-    registerBlockModelAsItem(block, meta, variantName, "");
-  }
+	public static void registerBlockModelAsItem(final Block block, int meta) {
+		registerBlockModelAsItem(block, meta, "inventory");
+	}
+	public static void registerBlockModelAsItem(final Block block, int meta, final String variantName) {
+		registerBlockModelAsItem(block, meta, variantName, "");
+	}
 
-  public static void registerBlockModelAsItem(final Block block, int meta, final String variantName, final String blockName) {
-	  Item item = Item.getItemFromBlock(block);
-	  ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName() + (blockName.equals("") ? "" : ("_" + blockName)), variantName));
-  }
+	public static void registerBlockModelAsItem(final Block block, int meta, final String variantName, final String blockName) {
+		Item item = Item.getItemFromBlock(block);
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName() + (blockName.equals("") ? "" : ("_" + blockName)), variantName));
+	}
 
-  public static void registerItemModel(final Item item){
-    registerItemModel(item, "inventory");
-  }
+	public static void registerItemModel(final Item item){
+		registerItemModel(item, "inventory");
+	}
 
-  public static void registerItemModel(final Item item, final String variant){
-    registerItemModel(item, variant, 0);
-  }
+	public static void registerItemModel(final Item item, final String variant){
+		registerItemModel(item, variant, 0);
+	}
 
-  public static void registerItemModel(final Item item, final String variant, final int meta){
-    ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), variant));
-  }
+	public static void registerItemModel(final Item item, final String variant, final int meta){
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), variant));
+	}
 }
