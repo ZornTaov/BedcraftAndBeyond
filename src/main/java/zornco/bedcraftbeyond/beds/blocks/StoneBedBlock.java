35804fbbf7faf1ccb2ca19ce80e2b93f1f5fa4a9
ZornTaov <zeke_kjell2@yahoo.com>
2019-10-27 22:34:40 -0700

    Update StoneBedBlock.java
    fix hitboxes

--- src/main/java/zornco/bedcraftbeyond/beds/blocks/StoneBedBlock.java
+++ src/main/java/zornco/bedcraftbeyond/beds/blocks/StoneBedBlock.java
@@ -8,11 +8,26 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.DyeColor;
+import net.minecraft.state.properties.BedPart;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class StoneBedBlock extends BedBlock {
+   protected static VoxelShape voxelTop = Block.makeCuboidShape(0.0D, 3.0D, 0.0D, 16.0D, 9.0D, 16.0D);
+   protected static VoxelShape voxelUndersideWest = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 16.0D, 3.0D, 14.0D);
+   protected static VoxelShape voxelUndersideSouth = Block.makeCuboidShape(2.0D, 0.0D, 0.0D, 14.0D, 3.0D, 14.0D);
+   protected static VoxelShape voxelUndersideEast = Block.makeCuboidShape(0.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D);
+   protected static VoxelShape voxelUndersideNorth = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 16.0D);
+   protected static VoxelShape voxelNorth = VoxelShapes.or(voxelTop, voxelUndersideNorth);
+   protected static VoxelShape voxelSouth = VoxelShapes.or(voxelTop, voxelUndersideSouth);
+   protected static VoxelShape voxelWest = VoxelShapes.or(voxelTop, voxelUndersideWest);
+   protected static VoxelShape voxelEast = VoxelShapes.or(voxelTop, voxelUndersideEast);
 
-    private static Block.Properties BED_PROPS = Block.Properties.create(Material.ROCK, MaterialColor.WHITE_TERRACOTTA);
+    private static Block.Properties BED_PROPS = Block.Properties.create(Material.ROCK, MaterialColor.STONE);
 
     public StoneBedBlock() {
         super(DyeColor.GRAY, BED_PROPS);
@@ -20,10 +35,25 @@ public class StoneBedBlock extends BedBlock {
     }
     @Override
     public BlockRenderType getRenderType(BlockState state) {
-      return BlockRenderType.MODEL;
+      return BlockRenderType.INVISIBLE;
    }
    @Override
    public void onLanded(IBlockReader worldIn, Entity entityIn) {
       entityIn.setMotion(entityIn.getMotion().mul(1.0D, 0.0D, 1.0D));
+   }
+   
+   public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+      Direction direction = state.get(HORIZONTAL_FACING);
+      Direction direction1 = state.get(PART) == BedPart.HEAD ? direction : direction.getOpposite();
+      switch(direction1) {
+      case NORTH:
+         return voxelNorth;
+      case SOUTH:
+         return voxelSouth;
+      case WEST:
+         return voxelWest;
+      default:
+         return voxelEast;
+      }
    }
 }
