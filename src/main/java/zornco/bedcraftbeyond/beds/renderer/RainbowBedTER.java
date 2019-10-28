c1736cfc7599f9822464d3bbfaf9e9586c678879
ZornTaov <zeke_kjell2@yahoo.com>
2019-10-27 17:30:46 -0700

    The Rainbow part of Raindbow Beds has been reimplemented!

--- src/main/java/zornco/bedcraftbeyond/beds/renderer/RainbowBedTER.java
+++ src/main/java/zornco/bedcraftbeyond/beds/renderer/RainbowBedTER.java
@@ -1,11 +1,13 @@
 package zornco.bedcraftbeyond.beds.renderer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.model.BedModel;
 import net.minecraft.item.DyeColor;
@@ -19,56 +21,74 @@ import zornco.bedcraftbeyond.core.BedCraftBeyond;
 /**
  * RainbowBedTER
  */
-public class RainbowBedTER extends TileEntityRenderer<RainbowBedTileEntity>{
-    private static final ResourceLocation[] TEXTURES = Arrays.stream(DyeColor.values()).sorted(Comparator.comparingInt(DyeColor::getId)).map((p_199742_0_) -> {
-       return new ResourceLocation("textures/entity/bed/" + p_199742_0_.getTranslationKey() + ".png");
-    }).toArray((p_199743_0_) -> {
-       return new ResourceLocation[p_199743_0_];
-    });
-    private final BedModel model = new BedModel();
+public class RainbowBedTER extends TileEntityRenderer<RainbowBedTileEntity> {
+   static ArrayList<Vector3f> colors = new ArrayList<Vector3f>();
+	static
+	{
+		for (int r=0; r<100; r++) colors.add(new Vector3f(r*255/100,       255,         0));
+		for (int g=100; g>0; g--) colors.add(new Vector3f(      255, g*255/100,         0));
+		for (int b=0; b<100; b++) colors.add(new Vector3f(      255,         0, b*255/100));
+		for (int r=100; r>0; r--) colors.add(new Vector3f(r*255/100,         0,       255));
+		for (int g=0; g<100; g++) colors.add(new Vector3f(        0, g*255/100,       255));
+		for (int b=100; b>0; b--) colors.add(new Vector3f(        0,       255, b*255/100));
+		                          colors.add(new Vector3f(        0,       255,         0));
+	}
+   private static final ResourceLocation[] TEXTURES = Arrays.stream(DyeColor.values())
+         .sorted(Comparator.comparingInt(DyeColor::getId)).map((p_199742_0_) -> {
+            return new ResourceLocation("textures/entity/bed/" + p_199742_0_.getTranslationKey() + ".png");
+         }).toArray((p_199743_0_) -> {
+            return new ResourceLocation[p_199743_0_];
+         });
+   private final BedModel model = new BedModel();
 
+   public void render(RainbowBedTileEntity tileEntityIn, double x, double y, double z, float partialTicks,
+         int destroyStage) {
+      if (destroyStage >= 0) {
+         this.bindTexture(DESTROY_STAGES[destroyStage]);
+         GlStateManager.matrixMode(5890);
+         GlStateManager.pushMatrix();
+         GlStateManager.scalef(4.0F, 4.0F, 1.0F);
+         GlStateManager.translatef(0.0625F, 0.0625F, 0.0625F);
+         GlStateManager.matrixMode(5888);
+      } else {
+         ResourceLocation resourcelocation = TEXTURES[tileEntityIn.getColor().getId()];
+         if (resourcelocation != null) {
+            this.bindTexture(resourcelocation);
+         }
+      }
+      if (tileEntityIn.hasWorld()) {
+         BlockState blockstate = tileEntityIn.getBlockState();
+         boolean isHead = blockstate.get(RainbowBedBlock.PART) == BedPart.HEAD;
+         this.func_199343_a(isHead, x, y, z,
+               blockstate.get(RainbowBedBlock.HORIZONTAL_FACING), isHead ? colors.get(tileEntityIn.getRainbowColor1()): colors.get(tileEntityIn.getRainbowColor2()));
+      } else {
+         this.func_199343_a(true, x, y, z, Direction.SOUTH, colors.get(tileEntityIn.getRainbowColor1()));
+         this.func_199343_a(false, x, y, z - 1.0D, Direction.SOUTH, colors.get(tileEntityIn.getRainbowColor2()));
+      }
 
-    public void render(RainbowBedTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage) {
-        if (destroyStage >= 0) {
-           this.bindTexture(DESTROY_STAGES[destroyStage]);
-           GlStateManager.matrixMode(5890);
-           GlStateManager.pushMatrix();
-           GlStateManager.scalef(4.0F, 4.0F, 1.0F);
-           GlStateManager.translatef(0.0625F, 0.0625F, 0.0625F);
-           GlStateManager.matrixMode(5888);
-        } else {
-           ResourceLocation resourcelocation = TEXTURES[tileEntityIn.getColor().getId()];
-           if (resourcelocation != null) {
-              this.bindTexture(resourcelocation);
-           }
-        }
-        if (tileEntityIn.hasWorld()) {
-           BlockState blockstate = tileEntityIn.getBlockState();
-           this.func_199343_a(blockstate.get(RainbowBedBlock.PART) == BedPart.HEAD, x, y, z, blockstate.get(RainbowBedBlock.HORIZONTAL_FACING));
-        } else {
-           this.func_199343_a(true, x, y, z, Direction.SOUTH);
-           this.func_199343_a(false, x, y, z - 1.0D, Direction.SOUTH);
-        }
-  
-        if (destroyStage >= 0) {
-           GlStateManager.matrixMode(5890);
-           GlStateManager.popMatrix();
-           GlStateManager.matrixMode(5888);
-        }
-  
-     }
-  
-     private void func_199343_a(boolean p_199343_1_, double p_199343_2_, double p_199343_4_, double p_199343_6_, Direction p_199343_8_) {
-        this.model.preparePiece(p_199343_1_);
-        GlStateManager.pushMatrix();
-        GlStateManager.translatef((float)p_199343_2_, (float)p_199343_4_ + 0.5625F, (float)p_199343_6_);
-        GlStateManager.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.translatef(0.5F, 0.5F, 0.5F);
-        GlStateManager.rotatef(180.0F + p_199343_8_.getHorizontalAngle(), 0.0F, 0.0F, 1.0F);
-        GlStateManager.translatef(-0.5F, -0.5F, -0.5F);
-        GlStateManager.enableRescaleNormal();
-        this.model.render();
-        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.popMatrix();
-     }
+      GlStateManager.color3f(1, 1, 1);
+      if (destroyStage >= 0) {
+         GlStateManager.matrixMode(5890);
+         GlStateManager.popMatrix();
+         GlStateManager.matrixMode(5888);
+      }
+
+   }
+
+   private void func_199343_a(boolean isHead, double x, double y, double z,
+         Direction direction, Vector3f color) {
+      this.model.preparePiece(isHead);
+      GlStateManager.pushMatrix();
+      GlStateManager.translatef((float) x, (float) y + 0.5625F, (float) z);
+      GlStateManager.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
+      GlStateManager.translatef(0.5F, 0.5F, 0.5F);
+      GlStateManager.rotatef(180.0F + direction.getHorizontalAngle(), 0.0F, 0.0F, 1.0F);
+      GlStateManager.translatef(-0.5F, -0.5F, -0.5F);
+      GlStateManager.enableRescaleNormal();
+      
+      GlStateManager.color3f(color.getX()/255.0F, color.getY()/255.0F, color.getZ()/255.0F);
+      this.model.render();
+      GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+      GlStateManager.popMatrix();
+   }
 }
--- src/main/java/zornco/bedcraftbeyond/beds/tileentity/RainbowBedTileEntity.java
+++ src/main/java/zornco/bedcraftbeyond/beds/tileentity/RainbowBedTileEntity.java
@@ -1,6 +1,7 @@
 package zornco.bedcraftbeyond.beds.tileentity;
 
 import net.minecraft.item.DyeColor;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
@@ -11,6 +12,7 @@ import zornco.bedcraftbeyond.core.BedcraftBlocks;
 
 public class RainbowBedTileEntity extends TileEntity implements ITickableTileEntity {
     private DyeColor color;
+    private int rainbowColor1 = 0, rainbowColor2 = 0;
 
     public RainbowBedTileEntity() {
        super(BedcraftBlocks.Rainbowbedte.get());
@@ -41,14 +43,48 @@ public class RainbowBedTileEntity extends TileEntity implements ITickableTileEnt
  
        return this.color;
     }
+    @Override
+    public CompoundNBT write(CompoundNBT nbttagcompound) {
+       super.write(nbttagcompound);
+       nbttagcompound.putInt("rainbowOffset1", getRainbowColor1());
+       nbttagcompound.putInt("rainbowOffset2", getRainbowColor2());
+       return nbttagcompound;
+    }
+    @Override
+    public void read(CompoundNBT nbttagcompound) {
+       super.read(nbttagcompound);
+       setRainbowColor1(nbttagcompound.getInt("rainbowOffset1"));
+       setRainbowColor2(nbttagcompound.getInt("rainbowOffset2"));
+    }
  
     public void setColor(DyeColor color) {
        this.color = color;
     }
+    public int incColor1() {
+       return this.setRainbowColor1(getRainbowColor1()+1);
+    }
+    public int incColor2() {
+       return this.setRainbowColor2(getRainbowColor2()+1);
+    }
+    public int getRainbowColor1() {
+       return rainbowColor1;
+    }
+    public int getRainbowColor2() {
+       return rainbowColor2;
+    }
+    public int setRainbowColor1(int color1) {
+       this.rainbowColor1 = color1 < 600?color1:0;
+       return this.rainbowColor1;
+    }
+    public int setRainbowColor2(int color1) {
+       this.rainbowColor2 = color1 < 600?color1:0;
+       return this.rainbowColor2;
+    }
 
     @Override
     public void tick() {
-        // TODO Auto-generated method stub
-
+        
+      incColor1();
+      incColor2();
     }
 }
